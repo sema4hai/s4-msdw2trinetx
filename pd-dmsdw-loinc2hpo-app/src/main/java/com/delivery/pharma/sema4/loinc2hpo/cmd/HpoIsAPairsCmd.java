fa3ce81242a389492f24da5251ae06f8198e0847
kingmanzhang <kingmanzhang@gmail.com>
2021-03-15 16:25:58 -0400

    implement calculating distances between is_a HPO pairs

--- pd-dmsdw-loinc2hpo-app/src/main/java/com/delivery/pharma/sema4/loinc2hpo/cmd/HpoIsAPairsCmd.java
+++ pd-dmsdw-loinc2hpo-app/src/main/java/com/delivery/pharma/sema4/loinc2hpo/cmd/HpoIsAPairsCmd.java
@@ -4,6 +4,7 @@ import com.beust.jcommander.Parameter;
 import com.beust.jcommander.Parameters;
 import com.delivery.pharma.sema4.loinc2hpo.MyUtils;
 import org.monarchinitiative.phenol.io.OntologyLoader;
+import org.monarchinitiative.phenol.ontology.algo.ShortestPathTable;
 import org.monarchinitiative.phenol.ontology.data.Ontology;
 import org.monarchinitiative.phenol.ontology.data.TermId;
 import org.slf4j.Logger;
@@ -55,21 +56,17 @@ public class HpoIsAPairsCmd implements Command {
         }
 
         Collection<TermId> terms = hpo.getAllTermIds();
-
+        ShortestPathTable shortestPathTable = new ShortestPathTable(hpo);
         for (TermId term : terms){
             Collection<TermId> ancestors = hpo.getAncestorTermIds(term, false);
             for (TermId ancestor : ancestors){
-                if (ancestor.equals(term)){
-                    continue;
-                } else {
-                    // TODO: calculate distance from current term to ancestor
-                    int distance = -1;
-                    try {
-                        writer.write(String.format("%s,%s,%d\n", term.getValue(), ancestor.getValue(), distance));
-                        logger.debug(String.format("%s, %s\n", hpo.getTermLabel(term).get(), hpo.getTermLabel(ancestor).get()));
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
+                int distance = shortestPathTable.getDistance(term, ancestor);;
+                try {
+                    writer.write(String.format("%s,%s,%d\n", term.getValue(), ancestor.getValue(), distance));
+                    logger.debug(String.format("%s, %s, %d\n", hpo.getTermLabel(term).get(),
+                            hpo.getTermLabel(ancestor).get(), distance));
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
             }
         }
