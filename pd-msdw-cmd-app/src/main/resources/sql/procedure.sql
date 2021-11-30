a38ee9bc732e061ebc14643895c426410981f1a4
kingmanzhang <kingmanzhang@gmail.com>
2021-11-30 09:31:58 -0500

    replace procedure table with production code

--- pd-msdw-cmd-app/src/main/resources/sql/procedure.sql
+++ pd-msdw-cmd-app/src/main/resources/sql/procedure.sql
@@ -1,15 +1,15 @@
 -- TODO: include newborn delivery
 -- TODO: ~20% "canceled" billing records are also noted as "completed". Right now, I consider a billing record valid as long as it is noted "completed"
-CREATE TABLE hai_az_test.procedure_dmsdw_2020July AS
+CREATE TABLE hai_az_prod.procedure_dmsdw_2020July AS
 WITH procedure_for_staging AS (
 SELECT DISTINCT dp.medical_record_number , f.encounter_key, f.caregiver_group_key, f.procedure_group_key,
 	bp.procedure_key , bp.procedure_rank ,
 	fdp.context_name , fdp.context_procedure_code , fdp.procedure_description , dc.calendar_date, bp.procedure_role,
 	dm.level1_context_name , dm.level2_event_name , dm.level3_action_name
-	FROM hai_az_test.fact f
+	FROM prod_msdw.fact f
 	JOIN prod_msdw.d_person dp on f.person_key = dp.person_key
 	JOIN prod_msdw.b_procedure bp on f.procedure_group_key = bp.procedure_group_key
-	JOIN hai_az_test.fd_procedure fdp on bp.procedure_key = fdp.procedure_key
+	JOIN hai_az_prod.fd_procedure fdp on bp.procedure_key = fdp.procedure_key
 	JOIN prod_msdw.d_metadata dm on f.meta_data_key = dm.meta_data_key
 	join prod_msdw.d_calendar dc using (calendar_key)
 	where
