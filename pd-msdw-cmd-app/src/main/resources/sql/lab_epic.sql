654297fd13a80df673f9213e95d346465f005bed
kingmanzhang <kingmanzhang@gmail.com>
2021-11-30 09:27:36 -0500

    replace epic lab with pseudo-production code

--- pd-msdw-cmd-app/src/main/resources/sql/lab_epic.sql
+++ pd-msdw-cmd-app/src/main/resources/sql/lab_epic.sql
@@ -1,12 +1,7 @@
 drop table if exists hai_az_test.lab_epic_2020July;
 CREATE TABLE hai_az_test.lab_epic_2020July as
 -- use a subset for testing
-with epic_lab as (
-    select el.*
-    from prod_msdw.epic_lab el
-    join ct_fabry."_cohort_by_dx" cbd using (mrn)
-),
-lab_epic_staging as (
+with lab_epic_staging as (
 	SELECT mrn, order_date , lab_time , test_code , test_result_value ,
 	case when trim(unit_of_measurement) in ('SHORT TEXT', 'SHORT DESCRIPTION', '') then NULL else trim(unit_of_measurement) end as unit_of_measurement,
 	reference_range,
@@ -18,12 +13,12 @@ lab_epic_staging as (
 	REPLACE(test_result_value_num, ',', '') :: REAL as test_result_value_final,
 	REPLACE(range_low, ',', '') :: REAL as range_low_final,
 	REPLACE(range_hi, ',', '') :: REAL  as range_hi_final
-	FROM epic_lab el
+	FROM prod_msdw.epic_lab el
 	where test_result_value ~ '^[0-9]+(\\.[0-9]+((,[0-9]+)?))?'
 	and reference_range ~ '^[0-9]+(\\.[0-9]+((,[0-9]+)?))?\\s?-\\s?[0-9]+(\\.[0-9]+((,[0-9]+)?))?'
 	-- the following line filters out a few records with ridiculouly large value
 	and length(REGEXP_SUBSTR(test_result_value , '\\-?[0-9,]+') ) < 30
 --	limit 1000
 )
-select mrn, order_date , lab_time , test_code , test_result_value , unit_of_measurement , reference_range , test_result_value_final, range_low_final, range_hi_final
+select mrn, order_date :: Date , lab_time , test_code , test_result_value , unit_of_measurement , reference_range , test_result_value_final, range_low_final, range_hi_final
 FROM lab_epic_staging ;
