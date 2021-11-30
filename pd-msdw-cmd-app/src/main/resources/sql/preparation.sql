bf74d5612f05703e365f2f56444604aed9f0ef9d
kingmanzhang <kingmanzhang@gmail.com>
2021-11-30 09:30:26 -0500

    replace fd_procedure with production code

--- pd-msdw-cmd-app/src/main/resources/sql/preparation.sql
+++ pd-msdw-cmd-app/src/main/resources/sql/preparation.sql
@@ -1,6 +1,6 @@
 -- there might be cases where one procedure_key is mapped to multiple entries
 -- it was a serious issue with earlier versions of DMSDW; not a big issue for MSDW
-CREATE TABLE hai_az_test.fd_procedure AS
+CREATE TABLE hai_az_prod.fd_procedure AS
 SELECT procedure_key, listagg(context_name, ' | ') within group (order by context_name , context_procedure_code ) as context_name ,
 listagg(context_procedure_code , ' | ') within group (order by context_name , context_procedure_code ) as context_procedure_code ,
 listagg(DISTINCT procedure_description, ' | ') within group (order by context_name , context_procedure_code ) as procedure_description
