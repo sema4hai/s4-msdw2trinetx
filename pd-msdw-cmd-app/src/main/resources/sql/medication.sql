014230d0bc4ff78e999447fe4a4939d66a831cf7
kingmanzhang <kingmanzhang@gmail.com>
2021-11-30 09:28:50 -0500

    replace med with production code

--- pd-msdw-cmd-app/src/main/resources/sql/medication.sql
+++ pd-msdw-cmd-app/src/main/resources/sql/medication.sql
@@ -36,7 +36,7 @@ SELECT dp.medical_record_number ,
 	LISTAGG(DISTINCT case when dm.level4_field_name = 'Route' then f.value end, '|') as Route,
 	LISTAGG(DISTINCT case when dm.level4_field_name = 'Site' then f.value end, '|') as Site,
 	LISTAGG(DISTINCT case when dm.level4_field_name = 'Administered Date' then f.value end, '|') as Administered_Date
-	FROM hai_az_test.fact f
+	FROM prod_msdw.fact f
 	JOIN prod_msdw.d_person dp on f.person_key = dp.person_key
 	JOIN prod_msdw.d_encounter de on f.encounter_key = de.encounter_key
 	JOIN prod_msdw.b_material bm on f.material_group_key = bm.material_group_key
@@ -104,7 +104,7 @@ SELECT dp.medical_record_number,
 	LISTAGG(DISTINCT case when dm.level4_field_name = 'Ordering Provider ID' then f.value end, '|') as Ordering_Provider_ID,
 	LISTAGG(DISTINCT case when dm.level4_field_name = 'Entered By' then f.value end, '|') as Entered_By,
 	LISTAGG(DISTINCT case when dm.level4_field_name = 'Start Date' then f.value end, '|') as Start_Date
-	FROM hai_az_test.fact f
+	FROM prod_msdw.fact f
 	JOIN prod_msdw.d_person dp on f.person_key = dp.person_key
 	JOIN prod_msdw.d_encounter de on f.encounter_key = de.encounter_key
 	JOIN prod_msdw.b_material bm on f.material_group_key = bm.material_group_key
@@ -160,7 +160,7 @@ SELECT dp.medical_record_number,
 	LISTAGG(DISTINCT case when dm.level4_field_name = 'Medication Route' then f.value end, '|') as Medication_Route,
 	LISTAGG(DISTINCT case when dm.level4_field_name = 'Administered Note' then f.value end, '|') as Administered_Note,
 	LISTAGG(DISTINCT case when dm.level4_field_name = 'Route Detail' then f.value end, '|') as Route_Detail
-	FROM hai_az_test.fact f
+	FROM prod_msdw.fact f
 	JOIN prod_msdw.d_person dp on f.person_key = dp.person_key
 	JOIN prod_msdw.d_encounter de on f.encounter_key = de.encounter_key
 	JOIN prod_msdw.b_material bm on f.material_group_key = bm.material_group_key
@@ -178,7 +178,7 @@ SELECT dp.medical_record_number,
 
 
 -- Create final table
-create table hai_az_test.medications_2020July as
+create table hai_az_prod.medications_2020July as
 With med_1 as (SELECT distinct medical_record_number,
     person_key,
     encounter_key,
@@ -186,7 +186,7 @@ With med_1 as (SELECT distinct medical_record_number,
     facility_key,
     caregiver_group_key,
     operation_key,
-    calendar_date,
+    calendar_date :: Date,
     time_of_day_key ,
 	  material_group_key,
 	  material_rank,
@@ -218,7 +218,7 @@ SELECT distinct medical_record_number,
     facility_key,
     caregiver_group_key,
     operation_key,
-    calendar_date,
+    calendar_date :: Date,
     time_of_day_key ,
 	  material_group_key,
 	  material_rank,
@@ -250,7 +250,7 @@ SELECT distinct medical_record_number,
     facility_key,
     caregiver_group_key,
     operation_key,
-    calendar_date,
+    calendar_date :: Date,
     time_of_day_key ,
 	  material_group_key,
 	  material_rank,
@@ -283,6 +283,7 @@ UNION ALL
 select distinct *
 FROM med_3;
 
-DROP VIEW IF EXISTS msdw_med_epic_admin;
-DROP VIEW IF EXISTS msdw_med_epic_prscb;
-DROP VIEW IF EXISTS msdw_med_tds_admin;
+
+drop view msdw_med_epic_admin;
+drop view msdw_med_epic_prscb;
+drop view msdw_med_tds_admin;
