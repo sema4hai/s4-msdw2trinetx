34c5f7bbd1c27394e8a848d2f8c0f69b5e02a49e
kingmanzhang <kingmanzhang@gmail.com>
2021-11-30 09:21:47 -0500

    replace lab with production code

--- pd-msdw-cmd-app/src/main/resources/sql/lab_scc.sql
+++ pd-msdw-cmd-app/src/main/resources/sql/lab_scc.sql
@@ -1,7 +1,7 @@
-CREATE TABLE hai_az_test.lab_scc_2020July AS
+CREATE TABLE hai_az_prod.lab_scc_2020July AS
 with
 	lab_flat as (
-		SELECT dp.medical_record_number , f.person_key , f.encounter_key , f.caregiver_group_key , f.facility_key , f.operation_key, dc.calendar_date , f.time_of_day_key,
+		SELECT dp.medical_record_number , f.person_key , f.encounter_key , f.caregiver_group_key , f.facility_key , f.operation_key, dc.calendar_date :: Date , f.time_of_day_key,
 		bp.procedure_group_key , bp.procedure_rank , bp.procedure_key , fdp.context_name , fdp.context_procedure_code , fdp.procedure_description ,
 		dm.level1_context_name , dm.level2_event_name , dm.level3_action_name ,
 		LISTAGG(case when dm.level4_field_name = 'Abnormal Flag' then f.value end, '|') as Abnormal_Flag,
@@ -18,10 +18,10 @@ with
 		LISTAGG(case when dm.level4_field_name = 'Clinical Result Numeric' then duom.unit_of_measure end, '|' ) as unit_of_measure_numeric,
 		LISTAGG(case when dm.level4_field_name = 'Clinical Result String' then duom.unit_of_measure end, '|' ) as unit_of_measure_string,
 		LISTAGG(case when dm.level4_field_name = 'Reference Range' then duom.unit_of_measure end, '|' ) as unit_of_measure_reference_range
-		FROM hai_az_test.fact_lab f
+		FROM prod_msdw.fact_lab f
 		JOIN prod_msdw.d_person dp on f.person_key = dp.person_key
 		JOIN prod_msdw.b_procedure bp on f.procedure_group_key = bp.procedure_group_key
-		JOIN hai_az_test.fd_procedure fdp on bp.procedure_key = fdp.procedure_key
+		JOIN hai_az_prod.fd_procedure fdp on bp.procedure_key = fdp.procedure_key
 		JOIN prod_msdw.d_metadata dm on f.meta_data_key = dm.meta_data_key
 		JOIN prod_msdw.d_unit_of_measure duom on f.uom_key = duom.uom_key
 		join prod_msdw.d_calendar dc using (calendar_key)
@@ -30,7 +30,7 @@ with
 		bp.procedure_group_key , bp.procedure_rank , bp.procedure_key , fdp.context_name , fdp.context_procedure_code , fdp.procedure_description ,
 		dm.level1_context_name , dm.level2_event_name , dm.level3_action_name
 	)
-SELECT medical_record_number , encounter_key , facility_key , calendar_date, time_of_day_key,
+SELECT medical_record_number , encounter_key , facility_key , calendar_date :: Date, time_of_day_key,
 	procedure_key  , Clinical_Result_Numeric, Clinical_Result_String, Reference_Range,  Abnormal_Flag, unit_of_measure_numeric,
 	case when unit_of_measure_string in ('SHORT TEXT', 'SHORT DESCRIPTION') then NULL else unit_of_measure_string end as unit_of_measure_string,
 	case when unit_of_measure_reference_range ~ 'SHORT TEXT' then NULL else unit_of_measure_reference_range end as unit_of_measure_reference_range
